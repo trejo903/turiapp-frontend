65130fa29aa1dbf28abc2bed26106f22627bfe11
Diego Trejo <124468131+trejo903@users.noreply.github.com>
2026-01-02 07:40:51 -0600

    Delete app/(tabs)/home/mapa/types.ts

--- app/(tabs)/home/mapa/types.ts
+++
@@ -1,7 +0,0 @@
-import { Sitio } from "@/src/lib/api";
-
-export type SitioWithImgs = Sitio & {
-  imagenes?: { id: number; url: string; orden?: number }[];
-};
-
-export type TravelMode = "DRIVING" | "WALKING";
